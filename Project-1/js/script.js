a30dafb04cee0f2e51b1ae40ae3023a4f636dfbf
Sam <50638220+samk87@users.noreply.github.com>
2019-05-16 21:29:53 -0500

    Updated 'citation' & 'year' to null out if empty.

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -1,20 +1,6 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
+// Array of quotes used to print to screen.
 let quotes = [
   {
   quote: "We have staked the whole future of our new nation, not upon the power of government; far from it. We have staked the future of all our political constitutions upon the capacity of each of ourselves to govern ourselves according to the moral principles of the Ten Commandments of God.",
@@ -33,27 +19,27 @@ let quotes = [
 {
   quote: "We can't help everyone, but everyone can help someone.",
   source: "Rondald Reagan.",
-  citation: " ",
-  year: " ",
+  citation: null,
+  year: null,
 },
 
 {
   quote: "A room without books is like a body without a soul.",
   source: "Marcus Tullius Cicero.",
-  citation: " ",
-  year: " ",
+  citation: null,
+  year: null,
 },
 
 {
   quote: "To live is the rarest thing in the world. Most people exist, that is all.",
   source: "Oscar Wilde.",
-  citation: " ",
-  year:" "
+  citation: null,
+  year: null,
 },
 
  ]
 
-// Use random numbers to generate quotes from array.
+// Function using random numbers to generate quotes from above array.
 function getRandomQuote() {
   let i = Math.floor(Math.random() * quotes.length);
         return quotes[i];
@@ -62,24 +48,13 @@ function getRandomQuote() {
 
 
 
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+// Function for printing random qotes to quote box.
 function printQuote(){
 let HTML = '';
 let i = getRandomQuote();
 
-HTML += '<p> class = "quote">' + i.quote + '</p>';
-  HTML += '<p class = "source">' + i.source + '</p>';
+HTML += '<p class = "quote">' + i.quote + '</p>';
+  HTML += '<p class = "source">' + i.source;
 
     if (i.year) {
       HTML += '<p class = "year">' + i.year + '</p>';
@@ -87,20 +62,9 @@ HTML += '<p> class = "quote">' + i.quote + '</p>';
       if (i.citation) {
         HTML += '<p class = "citation">' + i.citation + '</p>';
       }
-document.getElementById('quote-box').innerHTML = HTML;
+            document.getElementById('quote-box').innerHTML = HTML;
 }
 
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
+// "show another quote" button triggers the 'printQuote' function calling a random quote.
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
